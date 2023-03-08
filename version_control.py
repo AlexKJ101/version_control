16594241c9e26ed28836633526962e9fecc933db
BuildTools <unconfigured@null.spigotmc.org>
2023-03-08 18:15:24 -0500

    comment

--- version_control.py
+++ version_control.py
@@ -23,6 +23,8 @@ if __name__ == "__main__":
             input_password = input("Please enter your password to encode: ")
             encoded = encode(input_password)
             print("Your password has been encoded and stored!")
+        elif option == "2":
+
         else:
             i = 1
 
