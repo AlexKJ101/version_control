f0a6aa6a48bb5a67951ad7a4094107d0f734b8c9
FedeCB18 <127358667+FedeCB18@users.noreply.github.com>
2023-03-08 20:33:32 -0500

    Update version_control.py
    Changes made to Alexander Jamison by Federico Camargo

--- version_control.py
+++ version_control.py
@@ -1,13 +1,15 @@
-#Alexander Jamison
+# Alexander Jamison
+import numpy as np
+
 
 def encode(password):
     new_pass = ""
-    #Cycles through each digit of the original password and encodes it before storing it in a new variable
+    # Cycles through each digit of the original password and encodes it before storing it in a new variable
     for i in password:
         if int(i) < 7:
             new_pass = new_pass + str(int(i) + 3)
 
-        #The following are if statements so that the digits wrap back around instead of going into double digits
+        # The following are if statements so that the digits wrap back around instead of going into double digits
         elif i == "7":
             new_pass = new_pass + "0"
         elif i == "8":
@@ -15,47 +17,45 @@ def encode(password):
         else:
             new_pass = new_pass + "2"
 
-    #returns the newly encoded password
+    # returns the newly encoded password
     return new_pass
 
-#Function to decode the encoded password
+
+# Function to decode the encoded password
 def decode(encoded_pass):
+    lis = [int(x) for x in str(encoded_pass)]
+    arr = np.array(lis)
+    val = arr - 3
+    res = ''.join(map(str, val))
+    return res
 
 
+# Loops the menu so that the user can use the encoder/decoder more than once without restarting the program
+i = 0
+while i == 0:
 
-if __name__ == "__main__":
+    # Prints out a visual display of the menu
+    print("Menu \n------------- \n1. Encode \n2. Decode \n3. Quit ")
 
-    #Loops the menu so that the user can use the encoder/decoder more than once without restarting the program
-    i = 0
-    while i == 0:
+    # Asks the user to select which option they want to use for the program
+    option = input("Please enter an option: ")
 
-        #Prints out a visual display of the menu
-        print("Menu \n------------- \n1. Encode \n2. Decode \n3. Quit ")
+    if option == "1":
 
-        #Asks the user to select which option they want to use for the program
-        option = input("Please enter an option: ")
+        # Prompts user to input a password
+        input_password = input("Please enter your password to encode: ")
 
-        if option == "1":
+        # Runs the encode function to encode the password
+        encoded = encode(input_password)
+        print("Your password has been encoded and stored!")
 
-            #Prompts user to input a password
-            input_password = input("Please enter your password to encode: ")
+    elif option == "2":
 
-            #Runs the encode function to encode the password
-            encoded = encode(input_password)
-            print("Your password has been encoded and stored!")
+        # Decodes the previously encoded password and returns the original password
+        original = decode(encoded)
+        print(f"The encoded password is {encoded}, and the original password is {original}.")
 
-        elif option == "2":
+    else:
 
-            #Decodes the previously encoded password and returns the original password
-            original = decode(encoded)
-            print(f"The encoded password is {encoded}, and the original password is {original}.")
-
-        else:
-
-            #Ends the while loop and the program by setting i equal to a number other than 0
-            i = 1
-
-
-
-
-
+        # Ends the while loop and the program by setting i equal to a number other than 0
+        i = 1
